dd9861ba59f3c6eafcfc9c8018b49b18580573cd
Chris <christianbuchert@gmail.com>
2024-05-16 18:59:43 -0600

    Added links and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 function App() {
   return (
-    <div class={"h-full flex flex-col justify-center items-center gap-2"}>
-      <h1 class={"text-4xl font-bold text-gray-800"}>Amy Buchert</h1>
-      <p class={"text-gray-600"}>I make art.</p>
+    <div class={"h-full flex flex-col justify-center items-center gap-12"}>
+      <div class={"flex flex-col gap-2 justify-center items-center"}>
+        <h1 class={"text-5xl font-bold text-slate-800"}>Amy Buchert</h1>
+        <p class={"text-slate-600"}>I make art.</p>
+      </div>
+      <ul class={"flex gap-6"}>
+        <li>
+          <a
+            class={
+              "text-blue-500 hover:underline hover:text-blue-600 active:text-blue-700 transition-all"
+            }
+            href="https://www.artworkarchive.com/profile/amy-buchert"
+            target={"_blank"}
+          >
+            Portfolio and Contact
+          </a>
+        </li>
+        <li>
+          <a
+            class={
+              "text-blue-500 hover:underline hover:text-blue-600 active:text-blue-700 transition-all"
+            }
+            href="https://instagram.com/amybuchert"
+            target={"_blank"}
+          >
+            Follow me on Instagram
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
