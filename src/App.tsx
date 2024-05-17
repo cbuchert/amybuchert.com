920cc3779f7c39501db93a6a10cf630bd1fb436b
Chris <christianbuchert@gmail.com>
2024-05-16 18:48:34 -0600

    Added prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div class={"h-full flex flex-col justify-center items-center gap-2"}>
-     <h1 class={"text-4xl font-bold text-gray-800"}>Amy Buchert</h1>
-     <p class={"text-gray-600"}>I make art.</p>
+      <h1 class={"text-4xl font-bold text-gray-800"}>Amy Buchert</h1>
+      <p class={"text-gray-600"}>I make art.</p>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import App from "./App"
 
 import "./index.css"
 
-const root = document.getElementById('root')
+const root = document.getElementById("root")
 
 render(() => <App />, root!)
